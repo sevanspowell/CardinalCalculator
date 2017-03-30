b60fe9543c0f803c8becb8e5262fd64967251ee5
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-03-30 13:25:57 +1100

    Initial Commit

---
+++ CardinalCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardinalCalculator
+//
+//  Created by Samuel Evans-Powell on 30/3/17.
+//  Copyright © 2017 Samuel Evans-Powell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
