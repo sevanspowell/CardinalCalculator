95ca57ec9873be9b2a01fea649a8fa95e0de2204
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2017-04-03 14:17:34 +0800

    Improve comments and add resultIsPending property
    - Add comments to some CalculatorBrain methods and properties.
    - Add resultIsPending property indicating whether or not a binary operation is
    pending.

--- CardinalCalculator/CalculatorBrain.swift
+++ CardinalCalculator/CalculatorBrain.swift
@@ -10,7 +10,6 @@ import Foundation
 
 struct CalculatorBrain {
     
-    // Accumulator is optionalz
     private var accumulator: Double?
     
     private enum Operation {
@@ -33,6 +32,11 @@ struct CalculatorBrain {
         "="     : Operation.equals
     ]
     
+    /// Performs the operation corresponding to the `symbol`;
+    /// does nothing if no operation for `symbol`.
+    ///
+    /// - Parameter symbol: String such as "+", "*" or "√"
+    ///   corresponding to a mathematical operation.
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
@@ -51,9 +55,18 @@ struct CalculatorBrain {
                 performPendingBinaryOperation()
             }
         }
-
+        
     }
     
+    /// Returns a Boolean value indicating whether or not a
+    /// binary operation is pending.
+    var resultIsPending: Bool {
+        return pendingBinaryOperation != nil
+    }
+    
+    /// Performs the pending binary operation using the calculator's
+    /// accumulator as the second operand; does nothing if no pending
+    /// binary operation or no acumulator.
     private mutating func performPendingBinaryOperation() {
         if pendingBinaryOperation != nil && accumulator != nil {
             accumulator = pendingBinaryOperation!.perform(with: accumulator!)
@@ -63,6 +76,7 @@ struct CalculatorBrain {
     
     private var pendingBinaryOperation: PendingBinaryOperation?
     
+    /// An operand and binary operation awaiting a second operand.
     private struct PendingBinaryOperation {
         let function: (Double, Double) -> Double
         let firstOperand: Double
@@ -72,13 +86,14 @@ struct CalculatorBrain {
         }
     }
     
+    /// Set the calculator's current stored operand.
+    ///
+    /// - Parameter operand: new operand.
     mutating func setOperand(_ operand: Double) {
         accumulator = operand
     }
     
     var result: Double? {
-        get {
-            return accumulator
-        }
+        return accumulator
     }
 }
